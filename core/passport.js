a2ef8715a11c09a3697d83f709f21b136680ba80
seokhchoi <choiseok@oregonstate.edu>
2021-07-16 13:47:01 -0400

    More secure complex password
    1) handles server side code to toggle between securityRating/level 0 and 1
    2) For level 1 - requires that emails are valid format
    3) For level 1 - requires that passwords are long and complex
    4) For level 1 - some UI work related to feedback messages for bad email or pw

--- core/passport.js
+++ core/passport.js
@@ -1,7 +1,15 @@
 var db = require('../models')
 var LocalStrategy = require('passport-local').Strategy
 var bCrypt = require('bcrypt')
+var vh = require('./validationHandler')
 
+
+const pwLength = "- Must contain at least 8 characters<br>"
+const pwLower = "- Must contain at least 1 lowercase letter<br>"
+const pwUpper = "- Must contain at least 1 uppercase letter<br>"
+const pwNumber = "- Must contain at least 1 number<br>"
+const pwSpec = "- Must contain at least 1 special character"
+const badPWmsg = "Bad Password:<br>" + pwLength + pwLower + pwUpper + pwNumber + pwSpec
 
 module.exports = function (passport) {
 
@@ -60,21 +68,46 @@ module.exports = function (passport) {
                     if (user) {
                         return done(null, false, req.flash('danger', 'Account Already Exists'));
                     } else {
-                        if (req.body.email && req.body.password && req.body.username && req.body.cpassword && req.body.name) {
-                            if (req.body.cpassword == req.body.password) {
-                                db.User.create({
-                                    email: req.body.email,
-                                    password: createHash(password),
-                                    name: req.body.name,
-                                    login: username
-                                }).then(function (user) {
-                                    return done(null, user)
-                                })
+                        if(req.body.securityRating == 0){
+                            if (req.body.email && req.body.password && req.body.username && req.body.cpassword && req.body.name) {
+                                if (req.body.cpassword == req.body.password) {
+                                    db.User.create({
+                                        email: req.body.email,
+                                        password: createHash(password),
+                                        name: req.body.name,
+                                        login: username
+                                    }).then(function (user) {
+                                        return done(null, user)
+                                    })
+                                } else {
+                                    return done(null, false, req.flash('danger', 'Passwords do not match'));
+                                }
                             } else {
-                                return done(null, false, req.flash('danger', 'Passwords dont match'));
+                                return done(null, false, req.flash('danger', 'Input field(s) missing'));
+                            }   
+                        } else if(req.body.securityRating == 1){
+                            if(!vh.vEmail(req.body.email)){
+                                return done(null, false, req.flash('danger', 'Invalid Email'));
                             }
-                        } else {
-                            return done(null, false, req.flash('danger', 'Input field(s) missing'));
+                            if(!vh.vPassword(req.body.password)){
+                                return done(null, false, req.flash('danger', badPWmsg));
+                            }
+                            if (req.body.email && req.body.password && req.body.username && req.body.cpassword && req.body.name) {
+                                if (req.body.cpassword == req.body.password) {
+                                    db.User.create({
+                                        email: req.body.email,
+                                        password: createHash(password),
+                                        name: req.body.name,
+                                        login: username
+                                    }).then(function (user) {
+                                        return done(null, user)
+                                    })
+                                } else {
+                                    return done(null, false, req.flash('danger', 'Passwords do not match'));
+                                }
+                            } else {
+                                return done(null, false, req.flash('danger', 'Input field(s) missing'));
+                            }
                         }
                     }
                 });
