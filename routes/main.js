58c03ee680d3eb6c13045bfd30f780fb8f3c75dc
Elaine Laguerta <12928553+elaguerta@users.noreply.github.com>
2021-07-25 01:01:41 -0700

    feat: add 2fa routes

--- routes/main.js
+++ routes/main.js
@@ -16,7 +16,7 @@ module.exports = function (passport) {
 	router.get('/learn/vulnerability/:vuln', authHandler.isAuthenticated, function (req, res) {
 		var query_rating = req.query.securityRating ? req.query.securityRating : ratingState[req.params.vuln];
 		ratingState[req.params.vuln] = query_rating
-		if(req.params.vuln == 'a3_sensitive_data'){
+		if (req.params.vuln == 'a3_sensitive_data') {
 			ratingState['a5_broken_access_control'] = 0
 		}
 		res.render('vulnerabilities/layout', {
@@ -25,7 +25,7 @@ module.exports = function (passport) {
 			vuln_scenario: req.params.vuln + '/scenario',
 			vuln_description: req.params.vuln + '/description',
 			vuln_reference: req.params.vuln + '/reference',
-			vulnerabilities:vulnDict,
+			vulnerabilities: vulnDict,
 			ratings: ratingsDict[req.params.vuln],
 			securityRating: query_rating
 		}, function (err, html) {
@@ -39,12 +39,12 @@ module.exports = function (passport) {
 	})
 
 	router.get('/learn', authHandler.isAuthenticated, function (req, res) {
-		res.render('learn',{vulnerabilities:vulnDict})
+		res.render('learn', { vulnerabilities: vulnDict })
 	})
 
-	router.get('/register', authHandler.isNotAuthenticated, function (req, res){
+	router.get('/register', authHandler.isNotAuthenticated, function (req, res) {
 		var query_rating = req.query.securityRating ? req.query.securityRating : 0;
-    ratingState['register'] = query_rating;
+		ratingState['register'] = query_rating;
 		res.render('register', {
 			ratings: ratingsDict['register'],
 			securityRating: query_rating
@@ -67,39 +67,64 @@ module.exports = function (passport) {
 	router.get('/resetpw', authHandler.resetPw)
 
 	router.post('/login', passport.authenticate('login', {
-		successRedirect: '/learn',
 		failureRedirect: '/login',
 		failureFlash: true
-	}))
+	}), function (req, res) {
+		if (req.user.isTwoFactorAuthenticationEnabled) {
+			res.redirect('/auth2fa');
+		}
+		res.redirect('/learn');
+	}
+	)
 
-	router.post('/register', function(req, res, next){
+
+	router.post('/register', function (req, res, next) {
 		const rating = ratingState.register;
 		const redirect = rating == 2 ? '/setup2fa' : '/learn';
 		passport.authenticate('signup', {
-				successRedirect: redirect,
-				failureRedirect: '/register?securityRating=' + req.body.securityRating,
-				failureFlash: true
-			})(req, res, next);
+			successRedirect: redirect,
+			failureRedirect: '/register?securityRating=' + req.body.securityRating,
+			failureFlash: true
+		})(req, res, next);
 	})
-	
+
 	router.post('/forgotpw', authHandler.forgotPw)
 
-	router.post('/resetpw', authHandler.resetPwSubmit, function(req, res){
-		res.render('resetpw', {login: req.login, token: req.token, securityRating: req.securityRating})
+	router.post('/resetpw', authHandler.resetPwSubmit, function (req, res) {
+		res.render('resetpw', { login: req.login, token: req.token, securityRating: req.securityRating })
 	})
 
-  router.get('/setup2fa', authHandler.isAuthenticated, authHandler.generateTwoFactorAuthenticationCode, 
-    function(req, res){
-      res.render('setup2fa',{
-        login: req.user.login,
-        qrCodeURL: req.qrCodeURL
-      });
-  })
-  
-  router.post('/setup2fa', authHandler.turnOnTwoFactorAuthentication, function (req, res){
-		res.redirect('/learn')})
+	router.get('/setup2fa', authHandler.isAuthenticated, authHandler.generateTwoFactorAuthenticationCode,
+		function (req, res) {
+			res.render('setup2fa', {
+				login: req.user.login,
+				qrCodeURL: req.qrCodeURL
+			});
+		})
 
-  router.post('/2fa/generate', authHandler.generateTwoFactorAuthenticationCode);
+	router.post('/setup2fa', authHandler.turnOnTwoFactorAuthentication, function (req, res) {
+		res.redirect('/learn')
+	})
+
+	router.get('/auth2fa', function (req, res) {
+		res.render('auth2fa');
+	})
+	router.post('/auth2fa', async function (req, res) {
+		const user = req.user;
+		const { twoFactorAuthenticationCode } = req.body;
+		const isCodeValid = await authHandler.verifyTwoFactorAuthenticationCode(
+			twoFactorAuthenticationCode, user
+		);
+		if (isCodeValid) {
+			req.flash("2FA succeeded", true);
+			return res.redirect('/learn');
+		} else {
+			req.user = '';
+			res.send(401);
+		}
+	})
+
+	router.post('/2fa/generate', authHandler.generateTwoFactorAuthenticationCode);
 
 	router.post('/2fa/turn-on', authHandler.turnOnTwoFactorAuthentication);
 
