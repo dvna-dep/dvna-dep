50ed7881f8744ba66d7c06f6b28e2c5b238bb002
Elaine Laguerta <elaguerta@newrelic.com>
2021-07-26 22:08:16 -0700

    checkpoint

--- core/passport.js
+++ core/passport.js
@@ -1,158 +1,249 @@
-var db = require('../models')
-var LocalStrategy = require('passport-local').Strategy
-var bCrypt = require('bcrypt')
-var vh = require('./validationHandler')
-var authHandler = require('./authHandler.js')
+var db = require("../models");
+var LocalStrategy = require("passport-local").Strategy;
+var TotpStrategy = require("passport-totp").Strategy;
+var bCrypt = require("bcrypt");
+var vh = require("./validationHandler");
+var authHandler = require("./authHandler.js");
 
-
-const pwLength = "- Must contain at least 8 characters<br>"
-const pwLower = "- Must contain at least 1 lowercase letter<br>"
-const pwUpper = "- Must contain at least 1 uppercase letter<br>"
-const pwNumber = "- Must contain at least 1 number<br>"
-const pwSpec = "- Must contain at least 1 special character"
-const badPWmsg = "Bad Password:<br>" + pwLength + pwLower + pwUpper + pwNumber + pwSpec
+const pwLength = "- Must contain at least 8 characters<br>";
+const pwLower = "- Must contain at least 1 lowercase letter<br>";
+const pwUpper = "- Must contain at least 1 uppercase letter<br>";
+const pwNumber = "- Must contain at least 1 number<br>";
+const pwSpec = "- Must contain at least 1 special character";
+const badPWmsg =
+  "Bad Password:<br>" + pwLength + pwLower + pwUpper + pwNumber + pwSpec;
 
 module.exports = function (passport) {
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
 
-    passport.serializeUser(function (user, done) {
-        done(null, user.id)
+  passport.deserializeUser(function (uid, done) {
+    db.User.findOne({
+      where: {
+        id: uid,
+      },
+    }).then(function (user) {
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
     });
+  });
 
-    passport.deserializeUser(function (uid, done) {
-        db.User.findOne({
-            where: {
-                'id': uid
-            }
-        }).then(function (user) {
-            if (user) {
-                done(null, user);
-            } else {
-                done(null, false)
-            }
-
-        })
-    })
-    passport.use('login-2fa', new LocalStrategy({
-        passReqToCallback: true
-    },
-        async function (req, username, password, done) {
-            const user = await db.User.findOne({
-                where: {
-                    'login': username
-                }
-            });
-            if (!user) {
-                return done(null, false, req.flash('danger', 'Invalid Credentials'))
-            };
-            if (!isValidPassword(user, password)) {
-                return done(null, false, req.flash('danger', 'Invalid Credentials'))
-            };
-            return done(null, user);
+  passport.use(
+    new LocalStrategy(function (username, password, done) {
+      db.User.findOne({
+        where: {
+          login: username,
         },
-        async function (user, done) {
-            if (!user.isTwoFactorAuthenticationEnabled) {
-                return done(null, false, req.flash('danger', `2FA not setup for user ${user.name}`));
-            }
-            const { twoFactorAuthenticationCode } = req.body;
-            const isCodeValid = await authHandler.verifyTwoFactorAuthenticationCode(
-                twoFactorAuthenticationCode, user
-            );
-            if (isCodeValid) {
-                return done(null, user);
-            } else {
-                return (null, false, req.flash('danger', '2FA authentication failed'));
-            }
+      }).then(function (user) {
+        if (!user) {
+          return done(null, false, req.flash("danger", "Invalid Credentials"));
         }
-    ));
+        if (!isValidPassword(user, password)) {
+          return done(null, false, req.flash("danger", "Invalid Credentials"));
+        }
+        return done(null, user);
+      });
+    })
+  );
 
-    passport.use('login', new LocalStrategy({
-        passReqToCallback: true
-    },
-        function (req, username, password, done) {
-            db.User.findOne({
-                where: {
-                    'login': username
+  passport.use(
+    new TotpStrategy(function (user, done) {
+      var key = user.twoFactorAuthenticationCode;
+      if (!key) {
+        return done(new Error("No key"));
+      } else {
+        return done(null, key, 30); //30 = valid key period
+      }
+    })
+  );
+
+  // passport.use(
+  //   "login-2fa",
+  //   new LocalStrategy(
+  //     {
+  //       passReqToCallback: true,
+  //     },
+  //     async function (req, username, password, done) {
+  //       const user = await db.User.findOne({
+  //         where: {
+  //           login: username,
+  //         },
+  //       });
+  //       if (!user) {
+  //         return done(null, false, req.flash("danger", "Invalid Credentials"));
+  //       }
+  //       if (!isValidPassword(user, password)) {
+  //         return done(null, false, req.flash("danger", "Invalid Credentials"));
+  //       }
+  //       return done(null, user);
+  //     },
+  //     async function (user, done) {
+  //       if (!user.isTwoFactorAuthenticationEnabled) {
+  //         return done(
+  //           null,
+  //           false,
+  //           req.flash("danger", `2FA not setup for user ${user.name}`)
+  //         );
+  //       }
+  //       const { twoFactorAuthenticationCode } = req.body;
+  //       const isCodeValid = await authHandler.verifyTwoFactorAuthenticationCode(
+  //         twoFactorAuthenticationCode,
+  //         user
+  //       );
+  //       if (isCodeValid) {
+  //         return done(null, user);
+  //       } else {
+  //         return null, false, req.flash("danger", "2FA authentication failed");
+  //       }
+  //     }
+  //   )
+  // );
+
+  // passport.use(
+  //   "login",
+  //   new LocalStrategy(
+  //     {
+  //       passReqToCallback: true,
+  //     },
+  //     function (req, username, password, done) {
+  //       db.User.findOne({
+  //         where: {
+  //           login: username,
+  //         },
+  //       }).then(function (user) {
+  //         if (!user) {
+  //           return done(
+  //             null,
+  //             false,
+  //             req.flash("danger", "Invalid Credentials")
+  //           );
+  //         }
+  //         if (!isValidPassword(user, password)) {
+  //           return done(
+  //             null,
+  //             false,
+  //             req.flash("danger", "Invalid Credentials")
+  //           );
+  //         }
+  //         return done(null, user);
+  //       });
+  //     }
+  //   )
+  // );
+
+  var isValidPassword = function (user, password) {
+    return bCrypt.compareSync(password, user.password);
+  };
+
+  passport.use(
+    "signup",
+    new LocalStrategy(
+      {
+        passReqToCallback: true,
+      },
+      function (req, username, password, done) {
+        findOrCreateUser = function () {
+          db.User.findOne({
+            where: {
+              email: req.body.email,
+            },
+          }).then(function (user) {
+            if (user) {
+              return done(
+                null,
+                false,
+                req.flash("danger", "Account Already Exists")
+              );
+            } else {
+              if (req.body.securityRating == 0) {
+                if (
+                  req.body.email &&
+                  req.body.password &&
+                  req.body.username &&
+                  req.body.cpassword &&
+                  req.body.name
+                ) {
+                  if (req.body.cpassword == req.body.password) {
+                    db.User.create({
+                      email: req.body.email,
+                      password: createHash(password),
+                      name: req.body.name,
+                      login: username,
+                      isTwoFactorAuthenticationEnabled: false,
+                    }).then(function (user) {
+                      return done(null, user);
+                    });
+                  } else {
+                    return done(
+                      null,
+                      false,
+                      req.flash("danger", "Passwords do not match")
+                    );
+                  }
+                } else {
+                  return done(
+                    null,
+                    false,
+                    req.flash("danger", "Input field(s) missing")
+                  );
                 }
-            }).then(function (user) {
-                if (!user) {
-                    return done(null, false, req.flash('danger', 'Invalid Credentials'))
-                };
-                if (!isValidPassword(user, password)) {
-                    return done(null, false, req.flash('danger', 'Invalid Credentials'))
-                };
-                return done(null, user);
-            });
-        }));
+              } else if (req.body.securityRating >= 1) {
+                if (!vh.vEmail(req.body.email)) {
+                  return done(
+                    null,
+                    false,
+                    req.flash("danger", "Invalid Email")
+                  );
+                }
+                if (!vh.vPassword(req.body.password)) {
+                  return done(null, false, req.flash("danger", badPWmsg));
+                }
+                if (
+                  req.body.email &&
+                  req.body.password &&
+                  req.body.username &&
+                  req.body.cpassword &&
+                  req.body.name
+                ) {
+                  if (req.body.cpassword == req.body.password) {
+                    db.User.create({
+                      email: req.body.email,
+                      password: createHash(password),
+                      name: req.body.name,
+                      login: username,
+                      isTwoFactorAuthenticationEnabled: false,
+                    }).then(function (user) {
+                      req.user = user;
+                      return done(null, user);
+                    });
+                  } else {
+                    return done(
+                      null,
+                      false,
+                      req.flash("danger", "Passwords do not match")
+                    );
+                  }
+                } else {
+                  return done(
+                    null,
+                    false,
+                    req.flash("danger", "Input field(s) missing")
+                  );
+                }
+              }
+            }
+          });
+        };
+        process.nextTick(findOrCreateUser);
+      }
+    )
+  );
 
-    var isValidPassword = function (user, password) {
-        return bCrypt.compareSync(password, user.password);
-    }
-
-    passport.use('signup', new LocalStrategy({
-        passReqToCallback: true
-    },
-        function (req, username, password, done) {
-            findOrCreateUser = function () {
-                db.User.findOne({
-                    where: {
-                        'email': req.body.email
-                    }
-                }).then(function (user) {
-                    if (user) {
-                        return done(null, false, req.flash('danger', 'Account Already Exists'));
-                    } else {
-                        if (req.body.securityRating == 0) {
-                            if (req.body.email && req.body.password && req.body.username && req.body.cpassword && req.body.name) {
-                                if (req.body.cpassword == req.body.password) {
-                                    db.User.create({
-                                        email: req.body.email,
-                                        password: createHash(password),
-                                        name: req.body.name,
-                                        login: username,
-                                        isTwoFactorAuthenticationEnabled: false
-                                    }).then(function (user) {
-                                        return done(null, user)
-                                    })
-                                } else {
-                                    return done(null, false, req.flash('danger', 'Passwords do not match'));
-                                }
-                            } else {
-                                return done(null, false, req.flash('danger', 'Input field(s) missing'));
-                            }
-                        } else if (req.body.securityRating >= 1) {
-                            if (!vh.vEmail(req.body.email)) {
-                                return done(null, false, req.flash('danger', 'Invalid Email'));
-                            }
-                            if (!vh.vPassword(req.body.password)) {
-                                return done(null, false, req.flash('danger', badPWmsg));
-                            }
-                            if (req.body.email && req.body.password && req.body.username && req.body.cpassword && req.body.name) {
-                                if (req.body.cpassword == req.body.password) {
-                                    db.User.create({
-                                        email: req.body.email,
-                                        password: createHash(password),
-                                        name: req.body.name,
-                                        login: username,
-                                        isTwoFactorAuthenticationEnabled: false
-                                    }).then(function (user) {
-                                        req.user = user;
-                                        return done(null, user);
-                                    })
-                                } else {
-                                    return done(null, false, req.flash('danger', 'Passwords do not match'));
-                                }
-                            } else {
-                                return done(null, false, req.flash('danger', 'Input field(s) missing'));
-                            }
-                        }
-                    }
-                });
-            };
-            process.nextTick(findOrCreateUser)
-        }));
-
-    var createHash = function (password) {
-        return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
-    }
-
-}
+  var createHash = function (password) {
+    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+  };
+};
--- routes/main.js
+++ routes/main.js
@@ -1,139 +1,209 @@
-var router = require('express').Router()
-var vulnDict = require('../config/vulns')
-var ratingsDict = require('../config/ratings')
-var authHandler = require('../core/authHandler')
-var ratingState = require('../config/ratingState')
+var router = require("express").Router();
+var vulnDict = require("../config/vulns");
+var ratingsDict = require("../config/ratings");
+var authHandler = require("../core/authHandler");
+var ratingState = require("../config/ratingState");
 
 module.exports = function (passport) {
-	router.get('/', authHandler.isAuthenticated, function (req, res) {
-		res.redirect('/learn')
-	})
+  router.get(
+    "/",
+    authHandler.isAuthenticated,
+    authHandler.ensureTotp,
+    function (req, res) {
+      res.redirect("/learn");
+    }
+  );
 
-	router.get('/login', authHandler.isNotAuthenticated, function (req, res) {
-		var query_rating = req.query.securityRating ? req.query.securityRating : ratingState['login'];
-		ratingState['login'] = query_rating;
-		res.render('login', {
-			ratings: ratingsDict['login'],
-			securityRating: query_rating
-		})
-	})
+  router.get("/login", authHandler.isNotAuthenticated, function (req, res) {
+    var query_rating = req.query.securityRating
+      ? req.query.securityRating
+      : ratingState["login"];
+    ratingState["login"] = query_rating;
+    res.render("login", {
+      ratings: ratingsDict["login"],
+      securityRating: query_rating,
+    });
+  });
 
-	router.get('/learn/vulnerability/:vuln', authHandler.isAuthenticated, function (req, res) {
-		var query_rating = req.query.securityRating ? req.query.securityRating : ratingState[req.params.vuln];
-		ratingState[req.params.vuln] = query_rating
-		if (req.params.vuln == 'a3_sensitive_data') {
-			ratingState['a5_broken_access_control'] = 0
-		}
-		res.render('vulnerabilities/layout', {
-			vuln: req.params.vuln,
-			vuln_title: vulnDict[req.params.vuln],
-			vuln_scenario: req.params.vuln + '/scenario',
-			vuln_description: req.params.vuln + '/description',
-			vuln_reference: req.params.vuln + '/reference',
-			vulnerabilities: vulnDict,
-			ratings: ratingsDict[req.params.vuln],
-			securityRating: query_rating
-		}, function (err, html) {
-			if (err) {
-				console.log(err)
-				res.status(404).send('404')
-			} else {
-				res.send(html)
-			}
-		})
-	})
+  router.get(
+    "/learn/vulnerability/:vuln",
+    authHandler.isAuthenticated,
+    function (req, res) {
+      var query_rating = req.query.securityRating
+        ? req.query.securityRating
+        : ratingState[req.params.vuln];
+      ratingState[req.params.vuln] = query_rating;
+      if (req.params.vuln == "a3_sensitive_data") {
+        ratingState["a5_broken_access_control"] = 0;
+      }
+      res.render(
+        "vulnerabilities/layout",
+        {
+          vuln: req.params.vuln,
+          vuln_title: vulnDict[req.params.vuln],
+          vuln_scenario: req.params.vuln + "/scenario",
+          vuln_description: req.params.vuln + "/description",
+          vuln_reference: req.params.vuln + "/reference",
+          vulnerabilities: vulnDict,
+          ratings: ratingsDict[req.params.vuln],
+          securityRating: query_rating,
+        },
+        function (err, html) {
+          if (err) {
+            console.log(err);
+            res.status(404).send("404");
+          } else {
+            res.send(html);
+          }
+        }
+      );
+    }
+  );
 
-	router.get('/learn', authHandler.isAuthenticated, function (req, res) {
-		res.render('learn', { vulnerabilities: vulnDict })
-	})
+  router.get("/learn", authHandler.isAuthenticated, function (req, res) {
+    res.render("learn", { vulnerabilities: vulnDict });
+  });
 
-	router.get('/register', authHandler.isNotAuthenticated, function (req, res) {
-		var query_rating = req.query.securityRating ? req.query.securityRating : 0;
-		ratingState['register'] = query_rating;
-		res.render('register', {
-			ratings: ratingsDict['register'],
-			securityRating: query_rating
-		})
-	})
+  router.get("/register", authHandler.isNotAuthenticated, function (req, res) {
+    var query_rating = req.query.securityRating ? req.query.securityRating : 0;
+    ratingState["register"] = query_rating;
+    res.render("register", {
+      ratings: ratingsDict["register"],
+      securityRating: query_rating,
+    });
+  });
 
-	router.get('/logout', function (req, res) {
-		req.logout();
-		res.redirect('/');
-	})
+  router.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+  });
 
-	router.get('/forgotpw', function (req, res) {
-		var query_rating = req.query.securityRating ? req.query.securityRating : 0;
-		res.render('forgotpw', {
-			ratings: ratingsDict['forgotpw'],
-			securityRating: query_rating
-		});
-	})
+  router.get("/forgotpw", function (req, res) {
+    var query_rating = req.query.securityRating ? req.query.securityRating : 0;
+    res.render("forgotpw", {
+      ratings: ratingsDict["forgotpw"],
+      securityRating: query_rating,
+    });
+  });
 
-	router.get('/resetpw', authHandler.resetPw)
+  router.get("/resetpw", authHandler.resetPw);
 
-	router.post('/login', function (req, res) {
-		if (ratingState['login'] == 0) {
-			passport.authenticate('login', {
-				successRedirect: '/learn',
-				failureRedirect: '/login',
-				failureFlash: true
-			})(req, res);
-		} else {
-			console.log('81 call to isAuthenticated', req.isAuthenticated());
-			res.render('auth2fa', {
-				username: req.body.username,
-				password: req.body.password
-			});
-		}
-	})
+  router.post(
+    "/login",
+    passport.authenticate("local", { failureRedirect: "/login" }),
+    function (req, res) {
+      var rating = ratingState["login"];
+      var user = req.user;
+      if (rating == 0) {
+        // no totp
+        req.session.method = "plain";
+        res.redirect("/learn");
+      } else if (rating == 1) {
+        if (!user.isTwoFactorAuthenticationEnabled) {
+          req.flash(
+            "danger",
+            `Two Factor Authentication not enabled for user ${user.name}`
+          );
+        } else {
+          req.session.method = "totp";
+          res.redirect("/auth2fa");
+        }
+      }
+    }
+  );
 
+  // router.post("/login", function (req, res) {
+  //   if (ratingState["login"] == 0) {
+  //     passport.authenticate("login", {
+  //       successRedirect: "/learn",
+  //       failureRedirect: "/login",
+  //       failureFlash: true,
+  //     })(req, res);
+  //   } else {
+  //     res.render("auth2fa", {
+  //       username: req.body.username,
+  //       password: req.body.password,
+  //     });
+  //   }
+  // });
 
+  router.post("/register", function (req, res, next) {
+    const rating = ratingState.register;
+    const redirect = rating == 2 ? "/setup2fa" : "/learn";
+    passport.authenticate("signup", {
+      successRedirect: redirect,
+      failureRedirect: "/register?securityRating=" + req.body.securityRating,
+      failureFlash: true,
+    })(req, res, next);
+  });
 
-	router.post('/register', function (req, res, next) {
-		const rating = ratingState.register;
-		const redirect = rating == 2 ? '/setup2fa' : '/learn';
-		passport.authenticate('signup', {
-			successRedirect: redirect,
-			failureRedirect: '/register?securityRating=' + req.body.securityRating,
-			failureFlash: true
-		})(req, res, next);
-	})
+  router.post("/forgotpw", authHandler.forgotPw);
 
-	router.post('/forgotpw', authHandler.forgotPw)
+  router.post("/resetpw", authHandler.resetPwSubmit, function (req, res) {
+    res.render("resetpw", {
+      login: req.login,
+      token: req.token,
+      securityRating: req.securityRating,
+    });
+  });
 
-	router.post('/resetpw', authHandler.resetPwSubmit, function (req, res) {
-		res.render('resetpw', { login: req.login, token: req.token, securityRating: req.securityRating })
-	})
+  router.get(
+    "/setup2fa",
+    authHandler.generateTwoFactorAuthenticationCode,
+    function (req, res) {
+      res.render("setup2fa", {
+        login: req.user.login,
+        qrCodeURL: req.qrCodeURL,
+      });
+    }
+  );
 
-	router.get('/setup2fa', authHandler.isAuthenticated, authHandler.generateTwoFactorAuthenticationCode,
-		function (req, res) {
-			res.render('setup2fa', {
-				login: req.user.login,
-				qrCodeURL: req.qrCodeURL
-			});
-		})
+  router.post(
+    "/setup2fa",
+    authHandler.turnOnTwoFactorAuthentication,
+    function (req, res) {
+      req.flash(
+        "User successfully registered with 2fa. You will be redirected to the login page"
+      );
+      res.redirect("/learn");
+    }
+  );
 
-	router.post('/setup2fa', authHandler.turnOnTwoFactorAuthentication, function (req, res) {
-		res.redirect('/learn')
-	})
+  router.get("/auth2fa", authHandler.isAuthenticated, function (req, res) {
+    if (!req.user.isTwoFactorAuthenticationEnabled) {
+      req.flash(
+        "danger",
+        `Two Factor Authentication not enabled for user ${user.name}`
+      );
+      res.redirect("/login");
+    }
 
-	router.get('/auth2fa', function (req, res) {
-		res.render('auth2fa', {
-			username: req.body.username,
-			password: req.body.password
-		});
-	})
+    res.render("auth2fa", {
+      // TODO: take hidden inputs out of body
+      username: req.body.username,
+      password: req.body.password,
+    });
+  });
 
-	router.post('/auth2fa', function (req, res) {
-		passport.authenticate('login-2fa', {
-			successRedirect: '/learn',
-			failureRedirect: '/login',
-			failureFlash: true
-		})(req, res);
-	})
-	router.post('/2fa/generate', authHandler.generateTwoFactorAuthenticationCode);
+  router.post(
+    "/auth2fa",
+    authHandler.isAuthenticated,
+    passport.authenticate("totp", {
+      failureRedirect: "/login",
+      successRedirect: "/setup2fa",
+    })
+  );
 
-	router.post('/2fa/turn-on', authHandler.turnOnTwoFactorAuthentication);
+  // router.post("/auth2fa", authHandler.isAuthenticated, function (req, res) {
+  //   passport.authenticate("login-2fa", {
+  //     successRedirect: "/learn",
+  //     failureRedirect: "/login",
+  //     failureFlash: true,
+  //   })(req, res);
+  // });
+  router.post("/2fa/generate", authHandler.generateTwoFactorAuthenticationCode);
 
-	return router
-}
+  router.post("/2fa/turn-on", authHandler.turnOnTwoFactorAuthentication);
+
+  return router;
+};
